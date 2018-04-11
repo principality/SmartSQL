383be95c4f5cb1ab5fae83dcc0ac1e4c294119eb
principality <principality@home>
2018-04-12 01:03:04 +0800

    0411 add mysql client protocol test code

--- src/test/scala/me/principality/smartsql/protocol/MySQLSpec.scala
+++ src/test/scala/me/principality/smartsql/protocol/MySQLSpec.scala
@@ -3,6 +3,7 @@ package me.principality.smartsql.protocol
 import org.specs2.mutable
 import scala.concurrent.ExecutionContext.Implicits.global
 import slick.jdbc.JdbcBackend
+import slick.jdbc.MySQLProfile.api._
 
 class MySQLSpec extends mutable.Specification {
 
@@ -10,11 +11,13 @@ class MySQLSpec extends mutable.Specification {
   private val connection = "jdbc:mysql://127.0.0.1:33061/mysql"
 
   "connect to server" >> {
-    val db = JdbcBackend.Database.forURL(connection, driver = driver)
+    val db = JdbcBackend.Database.forURL(connection, driver = driver, user = "root", password = "123456")
     try {
-
-
+      val query = sql"select host, user from user".as[(String, String)]
+      val future = db.run(query)
+      future.onComplete{s => println(s)}
     } finally db.close()
+    success
   }
 
 }
